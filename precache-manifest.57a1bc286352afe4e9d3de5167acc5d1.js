565df1cb016775f3bf1c600d79fe16966e186ba8
ThinkTink <ThinkTinkEmail@protonmail.com>
2020-08-08 08:57:01 -0700

    Deploy

---
+++ precache-manifest.57a1bc286352afe4e9d3de5167acc5d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72b330ecfcbc5318bf50a1bfe9b9a84",
+    "url": "/BovineBookshelf/index.html"
+  },
+  {
+    "revision": "11c46b43141a3b934707",
+    "url": "/BovineBookshelf/static/js/2.99a2b604.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BovineBookshelf/static/js/2.99a2b604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6182d79560a2235d3c93",
+    "url": "/BovineBookshelf/static/js/main.3c453d28.chunk.js"
+  },
+  {
+    "revision": "dc7ccdae52610cd2f274",
+    "url": "/BovineBookshelf/static/js/runtime-main.2b7e539e.js"
+  },
+  {
+    "revision": "0e00cb245fba9761c1dacf7773306d84",
+    "url": "/BovineBookshelf/static/media/cow.0e00cb24.jpg"
+  }
+]);
